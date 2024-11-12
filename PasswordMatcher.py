eb2d539e53c3dfedc13b905e30007c0580aee89c
Oleksandr Danchenko <odanchen@uwaterloo.ca>
2024-11-12 16:41:07 -0500

    Created database access, wrote password matching with the database content

--- PasswordMatcher.py
+++ PasswordMatcher.py
@@ -1,9 +1,50 @@
+import mysql.connector
+import os
+
+
 class PasswordMatcher:
-    owner_password = None
+    __HOST = os.environ.get("DATA_HOST")
+    __USER = os.environ.get("DATABASE_USER_NAME")
+    __PASSWORD = os.environ.get("DATABASE_PASSWORD")
+    __DATABASE = os.environ.get("DATABASE_NAME")
+    __BOX_ID = os.environ.get("BOX_ID")
+
+    connection = None
+
     PASSWORD_LENGTH = 5
 
     def __init__(self):
-        self.owner_password = '10158'  # placeholder temporary password, will be read from database
+        self.connection = self.__establish_connection()
+
+    def __establish_connection(self):
+        try:
+            connection = mysql.connector.connect(host=self.__HOST, user=self.__USER, password=self.__PASSWORD,
+                                                 database=self.__DATABASE)
+            return connection
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
 
     def is_owner_password(self, password):
-        return password == self.owner_password
+        result = False
+        cursor = None
+        try:
+            cursor = self.connection.cursor()
+            query = """
+            SELECT Owner_passcode
+            FROM Mailboxes
+            WHERE Box_id = {}
+            """.format(self.__BOX_ID)
+
+            cursor.execute(query)
+            results = cursor.fetchone()
+
+            if results is not None:
+                result = results[0] == password
+
+        except mysql.connector.Error as err:
+            print(f"Query Error: {err}")
+        finally:
+            if cursor:
+                cursor.close()
+            return result
