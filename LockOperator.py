99f121dc010ce9ef781712d6ca8aae332eff7519
Oleksandr Danchenko <odanchen@uwaterloo.ca>
2024-11-15 18:24:10 -0500

    Added testing code for lock status

--- LockOperator.py
+++ LockOperator.py
@@ -10,6 +10,9 @@ except ImportError:
 
 class LockOperator:
     __LOCK_PIN = 11
+    __LOCK_CHECK_OUT = 19
+    __LOCK_CHECK_IN = 26
+    LOW = 0
 
     def __init__(self):
         GPIO.setwarnings(False)
@@ -17,7 +20,19 @@ class LockOperator:
         GPIO.setup(self.__LOCK_PIN, GPIO.OUT)
         GPIO.output(self.__LOCK_PIN, GPIO.LOW)
 
+        GPIO.setup(self.__LOCK_CHECK_OUT, GPIO.OUT)
+        GPIO.output(self.__LOCK_CHECK_OUT, GPIO.LOW)
+
+        GPIO.setup(self.__LOCK_CHECK_IN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
     def unlock_door(self):
         GPIO.output(self.__LOCK_PIN, GPIO.HIGH)
         sleep(0.1)
         GPIO.output(self.__LOCK_PIN, GPIO.LOW)
+
+    def is_lock_open(self):
+        GPIO.output(self.__LOCK_CHECK_OUT, GPIO.HIGH)
+        sleep(0.1)
+        ans = GPIO.input(self.__LOCK_CHECK_IN) == self.LOW
+        GPIO.output(self.__LOCK_CHECK_OUT, GPIO.LOW)
+        return ans
--- Main.py
+++ Main.py
@@ -19,13 +19,14 @@ def printPassword(password, current_len):
 
 while not status:
     if not ((n := reader.read()) is None):
+        print(operator.is_lock_open())
         current_password += n
         print(current_password)
         if len(current_password) == maxlen:
             if matcher.is_owner_password(current_password):
                 status = True
                 print('The password is correct!')
-                operator.unlock_door()
+                #operator.unlock_door()
             else:
                 print('Wrong password')
                 current_password = ""
