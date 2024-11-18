dcd3ab84b690dcfd23e729bd631f7b1a4b7b2bf0
Oleksandr Danchenko <odanchen@uwaterloo.ca>
2024-11-18 10:59:16 -0500

    fixed typo

--- OrderData.py
+++ OrderData.py
@@ -1,11 +1,11 @@
 class OrderData:
     pincode = None
-    order_Item = None
+    order_item = None
     open_status = None
     box_id = None
 
     def __init__(self, pincode, order_item, open_status, box_id):
         self.pincode = pincode
-        self.order_Item = order_item
+        self.order_item = order_item
         self.open_status = open_status
         self.box_id = box_id
