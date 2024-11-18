74c39bc8451512848b6b8bb7091990678a2f62c2
Oleksandr Danchenko <odanchen@uwaterloo.ca>
2024-11-18 10:56:04 -0500

    fixed data formatting

--- OrderData.py
+++ OrderData.py
@@ -4,8 +4,8 @@ class OrderData:
     open_status = None
     box_id = None
 
-    def __init__(self, query_result):
-        self.pincode = query_result.get("Pincode")
-        self.order_Item = query_result.get("Order_Item")
-        self.open_status = query_result.get("Open_status")
-        self.box_id = query_result.get("Box_id")
+    def __init__(self, pincode, order_item, open_status, box_id):
+        self.pincode = pincode
+        self.order_Item = order_item
+        self.open_status = open_status
+        self.box_id = box_id
--- PasswordMatcher.py
+++ PasswordMatcher.py
@@ -84,5 +84,5 @@ class PasswordMatcher:
             """.format(self.__BOX_ID, password))
         if not result:
             return None
-        return OrderData(result[0])
+        return OrderData(result[0][0], result[0][1], result[0][2], result[0][3])
 
