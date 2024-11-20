c336d72cf901bb67dd9ee30826b771ddf4208a59
estanzoo <mysupppermail@gmail.com>
2024-11-20 16:10:55 -0500

    sound test 1

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+from num2words import num2words
+from subprocess import call
 from KeypadReader import KeypadReader
 from PasswordMatcher import PasswordMatcher
 from LockOperator import LockOperator
@@ -10,14 +12,20 @@ current_password = ""
 maxlen = matcher.PASSWORD_LENGTH
 status = False
 
+cmd_beg= 'espeak '
+cmd_end= ' 2>/dev/null'
+
 while not status:
     if not ((n := reader.read()) is None):
+        call([cmd_beg+n+cmd_end], shell=True)
         current_password += n
         print(current_password)
         if len(current_password) == maxlen:
             if matcher.is_owner_password(current_password):
                 status = True
                 print('The password is correct!')
+                cmd = 'The password is correct!'
+                call([cmd_beg+cmd+cmd_end], shell=True)
                 # operator.unlock_door()
             elif (order_data := matcher.get_order_info(current_password)) is not None:
                 print('an order password is entered for: ')
